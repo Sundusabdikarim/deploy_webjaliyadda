028a169430dde108f6d757e2062f956243f3b0d1
Sundus Abdikarim <sundusabdikarim32@gmail.com>
2025-08-27 03:06:12 -0700

    image

--- app/_components/AboutUs/MissionVission.jsx
+++ app/_components/AboutUs/MissionVission.jsx
@@ -4,7 +4,7 @@ const MissionVision = ({ data }) => {
     <section className="misvis">
       {data.map((item, index) => (
         <div key={index} className={`row ${index % 2 === 1 ? "reverse" : ""}`}>
-          <img src={item.featuredImage} alt={item.headline} />
+          <Image src={item.featuredImage} alt={item.headline} />
           <div className="text">
             <h5>{item.headline}</h5>
             <p className="pragraph">{item.text}</p>
