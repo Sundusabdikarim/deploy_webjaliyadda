b9dd9f8970fb51281e014fb2b5a8f4683fd9dec9
Sundus Abdikarim <sundusabdikarim32@gmail.com>
2025-08-28 07:30:52 -0700

    images

--- app/_components/Events/image.jsx
+++ app/_components/Events/image.jsx
@@ -1,14 +1,20 @@
-import { getImageUrl } from "@/utilis/starpi.utlis";
+"use client"; // Needed if using Next.js Image on the client side
 
 import Image from "next/image";
+import { getImageUrl } from "@/utilis/starpi.utlis";
 
-const Images = ({component}) => {
-  
-    return (
-        <div className="imageIm">
-            <Image src={getImageUrl(component.featuredImage)} width={500} height={500} />
+const Images = ({ component }) => {
+  const imageUrl = getImageUrl(component.featuredImage);
 
-</div>
-    )
-}
+  return (
+    <div className="imageIm">
+      {imageUrl ? (
+        <Image src={imageUrl} width={500} height={500} alt="Event image" />
+      ) : (
+        <div className="placeholder">No Image</div>
+      )}
+    </div>
+  );
+};
+
 export default Images;
